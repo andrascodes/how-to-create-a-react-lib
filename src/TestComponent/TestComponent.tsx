5c03c16c7525a44d9308d42943e180f83ef4fd91
András Szücs <andrew0szucs@gmail.com>
2020-07-20 23:39:26 +0200

    Set up example component and add Emotion

---
+++ src/TestComponent/TestComponent.tsx
@@ -0,0 +1,21 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+export interface TestComponentProps {
+    color: "darkgreen" | "lightgreen";
+}
+
+const TestComponent: React.FC<TestComponentProps> = ({ color }) => (
+    <div
+    css={css`
+      background-color: hotpink;
+      &:hover {
+        color: ${color};
+      }
+  `}
+  >
+    This is a {color} component with a hotpink background.
+  </div>
+);
+
+export default TestComponent;
